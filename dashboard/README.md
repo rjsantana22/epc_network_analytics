2d29321f5d0f35a615f969042f488e71f2e45caf
Raul Santana <rjsantana95@gmail.com>
2026-07-20 00:34:52 -0400

    Added Dashboard in Looker Studio

---
+++ dashboard/README.md
@@ -0,0 +1,4 @@
+
+
+
+`https://datastudio.google.com/s/mGcs87_ooJ4`
--- epc_nw/models/marts/facts/fct_sessions.sql
+++ epc_nw/models/marts/facts/fct_sessions.sql
@@ -23,15 +23,19 @@ sessions as (
         apn_key,
         area_key,
         plmn_key,
+        event_timestamp,
         date(event_timestamp) as session_date,
-        time(event_timestamp) as session_time,
-        count(distinct event_id) as total_events,
+        concat(
+            lpad(cast(extract(hour from event_timestamp) as string), 2, '0'), ':',
+            lpad(cast(extract(minute from event_timestamp) as string), 2, '0')
+        ) as session_time,
+         count(distinct event_id) as total_events,
         count(distinct case when event_type = 'ATTACH' then event_id end) as attach_count,
         count(distinct case when event_type = 'HANDOVER' then event_id end) as handover_count,
         count(distinct case when event_type = 'PAGING' then event_id end) as paging_count,
         count(distinct case when event_type = 'SERVICE_REQUEST' then event_id end) as service_request_count
     from enriched
-    group by imsi, mme_id, apn_key, area_key, plmn_key, session_date, session_time
+    group by imsi, mme_id, apn_key, area_key, plmn_key, session_date, session_time, event_timestamp
 )
 
 select * from sessions
